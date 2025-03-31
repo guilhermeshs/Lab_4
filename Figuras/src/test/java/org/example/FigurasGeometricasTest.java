e19ae7c150eb9601863d86f3ecb1a2b6215a31de
Guilherme Henriques <dev.guilherme.henriques@gmail.com>
2025-03-31 11:51:30 -0300

    testeToString

--- Figuras/src/test/java/org/example/FigurasGeometricasTest.java
+++ Figuras/src/test/java/org/example/FigurasGeometricasTest.java
@@ -3,7 +3,6 @@ package org.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class FigurasGeometricasTest {
@@ -22,5 +21,13 @@ class FigurasGeometricasTest {
         assertEquals(3, figuras.getCirculo().getRaio(), 0.001);
         assertEquals(4, figuras.getRetangulo().getAlturaRetangulo(), 0.001);
         assertEquals(6, figuras.getRetangulo().getLarguraRetangulo(), 0.001);
+    }
+
+    @Test
+    void testToString() {
+        String expectedOutput = "Raio do circulo 3.0, Área de 28.27, Perímetro de 18.85\n" +
+                "Lado do quadrado 5.0, Area de 25.0, Perimetro de 20.0\n" +
+                "Altura do retangulo 4.0, Largura do retangulo 6.0, Area de 24.0, Perimetro de 20.0";
+        assertEquals(expectedOutput, figuras.toString());
     }
 }
